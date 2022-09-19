8a6a054e98371fb93fbb4b2904849163790a402c
Roman Pylypchuk <roman.pylypchuk@yahoo.com>
2022-09-19 22:51:11 +0300

    git commit

---
+++ src/main/java/cinema/service/impl/OrderServiceImpl.java
@@ -0,0 +1,32 @@
+package cinema.service.impl;
+
+import cinema.dao.OrderDao;
+import cinema.lib.Inject;
+import cinema.lib.Service;
+import cinema.model.Order;
+import cinema.model.ShoppingCart;
+import cinema.model.User;
+import cinema.service.OrderService;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class OrderServiceImpl implements OrderService {
+    @Inject
+    private OrderDao orderDao;
+
+    @Override
+    public Order completeOrder(ShoppingCart shoppingCart) {
+        Order order = new Order();
+        order.setUser(shoppingCart.getUser());
+        order.setTickets(new ArrayList<>(shoppingCart.getTickets()));
+        order.setOrderDate(LocalDateTime.now());
+        return orderDao.add(order);
+    }
+
+    @Override
+    public List<Order> getOrdersHistory(User user) {
+        return orderDao.getByUser(user);
+    }
+}
